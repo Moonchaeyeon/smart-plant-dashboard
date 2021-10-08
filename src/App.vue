3d1c52d5d3ea9b14671dfcf57a631b51eabf3e5a
Moonchaeyeon <sso07069@gmail.com>
2021-10-09 00:05:59 +0900

    hello

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="title"></div>
+<SideBar></SideBar>
+<router-view></router-view>
+</template>
+
+<script>
+import SideBar from "./components/SideBar.vue"; 
+export default {
+  name: 'App',
+  components: {
+    SideBar: SideBar,
+  }
+}
+</script>
+
+<style>
+.title {
+  width: 100%;
+  height: 60px;
+  background-color: #1a233a;
+}
+.side-bar {
+  position: absolute;
+  top: 0%;
+  width: 250px;
+  height: 1000px;
+  background-color: #1a233a;
+  color: #c8d9f8;
+  text-align: left;
+  padding: 10px;
+}
+
+.menu1 {
+  color: aqua;
+  padding-left: 20px;
+}
+
+.menu2 {
+  margin: 20px;
+  padding-left: 20px;
+  padding-bottom: 10px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="side-bar">
+    <div style="color: white; padding:20px; text-align:center; font-size:20px;">Smart M</div>
+    <div class="menu">
+        <div class="menu1">대시보드</div>
+        <div class="menu2">
+            <p @click="$router.push('/detection')">지능형 이상감지</p>
+            <p>CCTV 영상인식</p>
+            <p>IoT 모니터링</p>
+        </div>
+    </div>
+
+    <div class="menu">
+        <div class="menu1">IoT 관리</div>
+        <div class="menu2">
+            <p>센서 관리</p>
+            <p>센서 위치 등록</p>
+        </div>
+    </div>
+
+    <div class="menu">
+        <div class="menu1">e-SOP 관리</div>
+        <div class="menu2">
+            <p>e-SOP 에디터</p>
+            <p>담당자 관리</p>
+            <p>문자메시지 기록</p>
+        </div>
+    </div>
+
+    <div class="menu">
+        <div class="menu1">이상 감지 관리</div>
+        <div class="menu2">
+            <p>이상 경고 설정</p>
+            <p>이상 감지 이력</p>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'SideBar',
+}
+</script>
+
+<style>
+
+</style>
